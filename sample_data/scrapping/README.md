c8ba9d404b0278c0ab5a836807e398cac40c53e4
Wong Jia Hau <hou32hou@gmail.com>
2018-02-02 11:57:05 +0800

    90% completed code for scapper

--- sample_data/scrapping/README.md
+++ sample_data/scrapping/README.md
@@ -4,3 +4,9 @@
 npm install 
 node scrapper.js
 ```
+
+## How to get sources?
+Use the following links:
+```
+http://www.imdb.com/search/title?year=2015
+```
---
+++ sample_data/scrapping/scrapper.js
@@ -0,0 +1,58 @@
+const fs = require('fs'),
+    path = require('path'),
+    filePath = path.join(__dirname, 'sources/2015_imdb.html');
+
+const jsdom = require("jsdom");
+const {JSDOM} = jsdom;
+
+function Movie() {
+    this.title = null,
+    this.year = null,
+    this.genre = null,
+    this.img_path = null,
+    this.synopsis = null,
+    this.toString = () => {
+        return `${this.title},${this.year},${this.genre},${this.img_path},${this.synopsis}`
+    }
+}
+
+fs.readFile(filePath, {
+        encoding: 'utf-8'
+    }, function (err, data) {
+        if (!err) {
+            const dom = new JSDOM(data.toString());
+            const lists = (dom.window.document.getElementsByClassName("lister-item"));
+            const movies = [];
+            const keys = Object.keys(lists);
+            for (var i in keys) {
+                if(i === keys[keys.length - 1]) {
+                    break;
+                }
+                const item = lists[i].getElementsByClassName("lister-item-content")[0];
+                const x = new Movie();
+                x.title = quote(item.getElementsByClassName("lister-item-header")[0].getElementsByTagName("a")[0].innerHTML);
+                x.year = quote(filePath.split("/")[8].split("_")[0]);
+                x.genre = quote(item.getElementsByClassName("text-muted")[1].getElementsByClassName("genre")[0].innerHTML.trim());
+                x.img_path = quote(lists[i].getElementsByClassName("lister-item-image")[0].children[0].children[0].src);
+                x.synopsis = quote(item.getElementsByClassName("text-muted")[2].innerHTML.trim());
+                movies.push(x);
+            }
+            const header = "title, year, genre, img_path, synopsis\n";
+            const csv = header + movies.map((x) => x.toString()).join("\n");
+            
+            fs.writeFile(__dirname + "/output.csv", csv, function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+
+                console.log("The file was saved!");
+            }); 
+
+        } else {
+            console.log(err);
+        }
+    });
+
+function quote(s) {
+    return "\"" + s + "\"";
+}
