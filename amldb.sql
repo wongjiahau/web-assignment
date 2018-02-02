eb27372267eec58ce90be281b4b8d28f8f835b9d
Wong Jia Hau <hou32hou@gmail.com>
2018-02-02 13:49:33 +0800

    Updated schema

--- amldb.sql
+++ amldb.sql
@@ -12,8 +12,8 @@ create table video (
     video_id int not null auto_increment primary key,
     title varchar(100) not null,
     year int not null,
-    genre varchar(20) not null,
-    img_path varchar(100) not null,
+    genre varchar(100) not null,
+    img_path varchar(300) not null,
     synopsis varchar(1000)
 ) engine = InnoDB;
 
