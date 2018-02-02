9d4e17345199c62d579eb09805b7503e0629d374
Wong Jia Hau <hou32hou@gmail.com>
2018-02-02 09:57:07 +0800

    Fix syntax error in amldb.sql

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # web-assignment
+
+## How to load SQL script into MySql?
+```
+source ~/Repos/web-assignment/amldb.sql;
+```
+
--- amldb.sql
+++ amldb.sql
@@ -1,32 +1,36 @@
 -- aml stands for AAA Movie Library
+drop database aml;
 create database aml;
 use aml;
 
 create table admin_data (
-    password_hash varchar(50), -- hash of the admin password
-);
+    admin_id varchar(7) not null primary key,
+    password_hash varchar(50) -- hash of the admin password
+) engine = InnoDB;
 
 create table video (
     video_id int not null auto_increment primary key,
     title varchar(100) not null,
     year int not null,
     genre varchar(20) not null,
-    img_path varchar(100) not null
-    synopsis varchar(1000),
-);
+    img_path varchar(100) not null,
+    synopsis varchar(1000)
+) engine = InnoDB;
 
 -- optional table
 create table customer (
     customer_id int not null auto_increment primary key,
     uname varchar(50) not null
-);
+) engine = InnoDB;
 
 -- optional table
 create table rent (
     rent_id int not null auto_increment primary key,
-    video_id int,
-    customer_id int,
+    video_id int not null,
+    customer_id int not null,
     date datetime, 
-    foreign key (video_id) references video(video_id),
-    foreign key (customer_id) references video(customer_id)
-);
+    constraint `rent_ref_video_id` foreign key (video_id) references video(video_id),
+    constraint `rent_ref_customer_id` foreign key (customer_id) references customer(customer_id)
+) engine = InnoDB;
+
+show tables;
