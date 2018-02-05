3e4c394acc494e02c03ac3cb52c8b6d779ffe784
Wong Jia Hau <hou32hou@gmail.com>
2018-02-05 16:29:52 +0800

    Move list movie code to retrieve_move.php

--- home/admin/retrieve_movie.php
+++ home/admin/retrieve_movie.php
@@ -1,6 +1,20 @@
-
 <html>
     <body>
         <h1>retrieve movie</h1>
+        <?php
+        require($_SERVER['DOCUMENT_ROOT'] . "/home/util/connect.php");
+        $query = "select * from video;";
+        $result = mysqli_query($conn, $query);
+        if (!$result) {
+            $message = 'Invalid query: ' . mysqli_error() . "\n";
+            $message .= 'Whole query: ' . $query;
+            echo $message;
+            die($message);
+        }
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "" . $row['title'] . " <br>";
+        }
+        mysqli_close($conn);
+        ?>
     </body>
 </html>
--- home/index.php
+++ home/index.php
@@ -2,19 +2,6 @@
 	<body>
 		<header>Welcome to AAA Movie Library</header>
 		<?php
-	require("./util/connect.php");
-	$query = "select * from video;";
-	$result = mysqli_query($conn, $query);
-	if (!$result) {
-		$message = 'Invalid query: ' . mysqli_error() . "\n";
-		$message .= 'Whole query: ' . $query;
-		echo $message;
-		die($message);
-	}
-	while ($row = mysqli_fetch_assoc($result)) {
-		echo "".$row['title']." <br>";
-	}
-	mysqli_close($conn);
 	?>
 	</body>
 </html>
