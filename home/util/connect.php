0032b789c50ef207cc40d23611355e7b025cdd35
Wong Jia Hau <hou32hou@gmail.com>
2018-02-05 14:52:27 +0800

    Updated connect.php to not include db password

--- home/util/connect.php
+++ home/util/connect.php
@@ -1,12 +1,14 @@
 <?php
-	$dbhost = 'localhost';
-	$dbuser = 'root';
-	$dbpass = '842684268426';
-	$dbname = "aml";
-    $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-	if (!$conn) {
-		die('Could not connect: ' . mysqli_error());
-	} else {
-		echo 'Connected successfully to database<br>';
-    }
+$dbpass = file_get_contents("/dbpass.txt");
+$dbpass = substr($dbpass, 0, -1); // Because an invisible character need to be omitted 
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbname = "aml";
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+if (!$conn) {
+	echo "Error";
+	die('Could not connect: ' . mysqli_error());
+} else {
+	echo 'Connected successfully to database<br>';
+}
 ?>
