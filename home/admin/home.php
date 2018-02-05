f3041112f57887142ab5fc7ed4ab8db8e34e1c99
Wong Jia Hau <hou32hou@gmail.com>
2018-02-05 16:01:30 +0800

    Add more function to admin home

--- home/admin/home.php
+++ home/admin/home.php
@@ -1,5 +1,15 @@
+<?php
+function big_button($content, $url)
+{
+    echo "<a href='$url'>$content</a><br/>";
+}
+?>
 <html>
     <body>
         <h1>Welcome to Admin Home</h1>
+        <?php big_button("Add new movies", "404") ?>
+        <?php big_button("Delete movies", "404") ?>
+        <?php big_button("Edit movies", "404") ?>
+        <?php big_button("Browse movies", "404") ?>
     </body>
 </html>
