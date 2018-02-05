9b1719ea5a8a01da761392807f806fb329da3ff5
Wong Jia Hau <hou32hou@gmail.com>
2018-02-05 13:56:51 +0800

    Added login and logout page for admin

--- home/admin/login.php
+++ home/admin/login.php
@@ -1,9 +1,30 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '842684268426', 'aml');
+require('./../util/connect.php');
 if (!$conn) {
     die("Database connection failed." . mysqli_error($conn));
 }
 session_start();
+if (isset($_POST['username']) and isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $query = "SELECT * FROM `admin_data` WHERE admin_id='$username' AND password_hash='$password';";
+    $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+    $count = mysqli_num_rows($result);
+	while ($row = mysqli_fetch_assoc($result)) {
+		echo "".$row['title']." <br>";
+	}
+    if ($count == 1) {
+        $_SESSION['username'] = $username;
+    } else {
+        $error = "Invalid login credentials";
+    }
+    if(isset($_SESSION['username'])) {
+        $username = $_SESSION['username'];
+        echo "Hi".$usrname;
+        echo "<a href='logout.php'>Logout</a>";
+
+    }
+}
 ?>
 <html>
 
