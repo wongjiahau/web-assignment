c2b7aeca570618a103bdb13fd8e31d21acabb342
Wong Jia Hau <hou32hou@gmail.com>
2018-02-07 22:16:49 +0800

    Added sample for using ajax

--- home/admin/retrieve_movie.php
+++ home/admin/retrieve_movie.php
@@ -8,33 +8,62 @@ Properties of $movie:
 - img_path
 - synopsis
  */
+if (isset($_POST['search_word'])) {
+    echo "<br>";
+    echo "You searched for ".$_POST['search_word'];
+    exit;
+}
+
 function render_movie_item($movie)
 {
+    $id = $movie['video_id'];
     $title = $movie['title'];
     $year = $movie['year'];
     $genre = $movie['genre'];
     $img_path = $movie['img_path'];
     $synopsis = $movie['synopsis'];
     echo "
-    <table>
-        <tr>
-            <td>
-                <img src='$img_path'></img>
-            </td>
-            <td>
-                <h3>$title ($year)</h3>
-                Genre: $genre <br>
-                Synopsis: <article>$synopsis</article>
-            </td>
-        </tr>
-    </table> 
+    <div class='movieItem' id='movieItem$id'>
+        <table>
+            <tr>
+                <td>
+                    <img src='$img_path'></img>
+                </td>
+                <td>
+                    <h3>$title ($year)</h3>
+                    Genre: $genre <br>
+                    Synopsis: <article>$synopsis</article>
+                </td>
+            </tr>
+        </table> 
+    </div>
     ";
 
 }
 ?>
 <html>
+    <style>
+        .movieItem {
+            border: 1px solid black
+        }
+        
+    </style>
     <body>
-        <h1>retrieve movie</h1>
+        
+        <h1 id="header">retrieve movie</h1>
+        <input id="searchInput" type="text"> <input id="searchBtn" type="submit"> <br>
+        <p id="response"></p>
+        genre <select name="genre">
+            <option value="A">A</option>
+            <option value="B">A</option>
+            <option value="-">Other</option>
+        </select> <br>
+        year <select name="year">
+            <option value="A">A</option>
+            <option value="B">A</option>
+            <option value="-">Other</option>
+        </select>
+
         <?php
         require($_SERVER['DOCUMENT_ROOT'] . "/home/util/connect.php");
         $query = "select * from video;";
@@ -45,12 +74,26 @@ function render_movie_item($movie)
             echo $message;
             die($message);
         }
-        while ($row = mysqli_fetch_assoc($result)) {
+        $LIMIT = 10;
+        for ($i = 0; $i < $LIMIT; $i++) {
+            $row = mysqli_fetch_assoc($result);
             render_movie_item($row);
-            // echo json_encode($row)."<br>";
-            // echo "" . $row['title'] . " <br>";
         }
         mysqli_close($conn);
         ?>
     </body>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+    <script lang="js">
+        $('#searchBtn').click(function() {
+            $.ajax({
+                type: "POST",
+                url: "retrieve_movie.php",
+                data: { search_word: document.getElementById("searchInput").value}
+            }).done(function( ajaxResponse ) {
+                document.getElementById("response").innerHTML = ajaxResponse
+                console.log( "Data Saved: " + ajaxResponse );
+            });    
+        });
+        x = (1+2)/5;
+    </script>
 </html>
