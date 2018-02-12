dd5cc17cc6c819d3d022586e3c00b2c03c790100
Wong Jia Hau <hou32hou@gmail.com>
2018-02-12 16:18:09 +0800

    Added in index.php on how to require files properly.

--- home/index.php
+++ home/index.php
@@ -2,6 +2,10 @@
 	<body>
 		<header>Welcome to AAA Movie Library</header>
 		<?php
+define('__ROOT__', dirname(dirname(__FILE__)));
+require(__ROOT__.'/src/sample.php');
+$x = new Sample();
+echo $x->toString();
 	?>
 	</body>
 </html>
