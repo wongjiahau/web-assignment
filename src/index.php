bd8b649050ceccc02e285f885ad9367eda41d50d
Wong Jia Hau <hou32hou@gmail.com>
2018-02-21 16:38:38 +0800

    Included dbconfig

--- src/index.php
+++ src/index.php
@@ -1,7 +1,13 @@
 <?php
 require './libs/bootstrap.php';
 require './libs/controller.php';
-require './libs/view.php';
+require './libs/database.php';
 require './libs/model.php';
+require './libs/navigator.php';
+require './libs/session.php';
+require './libs/view.php';
+
+require './config/paths.php';
+require './config/dbconfig.php';
 
 $app = new Bootstrap();
--- src/libs/database.php
+++ src/libs/database.php
@@ -3,6 +3,6 @@ class Database extends PDO
 {
     public function __construct()
     {
-        parent::__construct('mysql:host=localhost; dbname=aml', 'root', 'root');
+        parent::__construct(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
     }
 }
