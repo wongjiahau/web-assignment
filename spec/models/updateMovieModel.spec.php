47a84f9d42152eb49b1baabe1e8a4bb7401d7714
Wong Jia Hau <hou32hou@gmail.com>
2018-03-01 18:24:48 +0800

    Past test for updateMovieModel

--- spec/models/updateMovieModel.spec.php
+++ spec/models/updateMovieModel.spec.php
@@ -18,4 +18,50 @@ describe("updateMovieModel", function () {
             expect($res->title)->toBe('The Walking Dead');
         });
     });
+
+    describe("run", function() {
+        it("should update img_path if img_path is not null", function() {
+            $x = new UpdateMovieModel();
+            $x->run("98", new Movie(
+                "new title",
+                "9999",
+                "new genre",
+                "new img_path",
+                "new synopsis"
+            ));
+            $res = $x->queryDb("select * from video where video_id = 98");
+            unset($res[0]['ts']);
+            expect($res[0])->toBe(array(
+               "video_id" => "98",
+               "title" => "new title",
+               "year" => "9999",
+               "genre" => "new genre",
+               "img_path" => "new img_path",
+               "synopsis" => "new synopsis"
+            ));
+        });
+
+        it("should not update img_path if img_path is null", function() {
+            $ORIGINAL_IMG_PATH = "https://images-na.ssl-images-amazon.com/images/M/MV5BNzBiYWVjMjgtMTUzYy00YTFlLWE2OWYtMzY3ZmNmYTE0ODg5XkEyXkFqcGdeQXVyNjMxNzcwOTI@._V1_UX67_CR0,0,67,98_AL_.jpg";
+            $x = new UpdateMovieModel();
+            $x->run("99", new Movie(
+                "new title",
+                "9999",
+                "new genre",
+                null,                
+                "new synopsis"
+            ));
+            $res = $x->queryDb("select * from video where video_id = 99");
+            unset($res[0]['ts']);
+            expect($res[0])->toBe(array(
+               "video_id" => "99",
+               "title" => "new title",
+               "year" => "9999",
+               "genre" => "new genre",
+               "img_path" => $ORIGINAL_IMG_PATH,
+               "synopsis" => "new synopsis"
+            ));
+
+        });
+    });
 });
--- src/models/updateMovieModel.php
+++ src/models/updateMovieModel.php
@@ -15,4 +15,26 @@ class UpdateMovieModel extends Model
         return json_encode($res[0]);
     }
 
+    public function run($video_id, $movie)
+    {
+        $this->queryDb(
+            "update video 
+            set title = :title,
+                year = :year,
+                genre = :genre,
+                synopsis = :synopsis,
+                img_path = COALESCE( :img_path, img_path)
+                where video_id = :video_id",
+            array(
+                ":title" => $movie->title,
+                ":year" => $movie->year,
+                ":genre" => $movie->genre,
+                ":synopsis" => $movie->synopsis,
+                ":img_path" => $movie->img_path,
+                ":video_id" => (int)$video_id
+            )
+        );
+    }
+
+
 }
